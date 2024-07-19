47f3042c86e4205cd09c1d89a84c0cd1f29dc57d
reignkk1 <ruaals456@naver.com>
2024-07-19 23:20:11 +0900

    feat: 댓글 마크업

--- comments.js
+++ comments.js
@@ -1,7 +1,24 @@
+import { createElement } from "./utils.js";
+
 const commentsContainer = document.querySelector(".comments");
 
-// 댓글쓰기 렌더링
-function renderWriteComment() {}
+renderWriteComment();
+renderCommentsList();
 
-// 댓글들 렌더링
-function renderComments() {}
+// 댓글쓰기 렌더링
+function renderWriteComment() {
+  const commentWrite = createElement("div", { class: "comment-write" });
+  commentsContainer.appendChild(commentWrite);
+}
+
+// 댓글 리스트 렌더링
+function renderCommentsList() {
+  const commentsList = createElement("ul", { class: "comments-list" });
+  const comment = createElement("li", { class: "comment" });
+  const nickname = createElement("div", { class: "nickname" });
+  const content = createElement("p", { class: "content" });
+
+  comment.append(nickname, content);
+  commentsList.appendChild(comment);
+  commentsContainer.appendChild(commentsList);
+}
