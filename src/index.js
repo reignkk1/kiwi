1160b83e7b0519dd0065359b3d5338aaaa16d377
reignkk1 <ruaals456@naver.com>
2024-08-21 23:22:31 +0900

    feat: comment-list, text-area, button-container 컴포넌트 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import AudioScreen from "./components/AudioScreen.js";
-import Comment from "./components/Comment.js";
-import "./reset.css";
+import AudioScreen from './components/AudioScreen.js';
+import Comment from './components/comment/index.js';
+import Buttons from './components/comment/Buttons.js';
+import TextArea from './components/comment/TextArea.js';
+import CommentList from './components/comment/CommentList.js';
 
-customElements.define("audio-screen", AudioScreen);
-customElements.define("comment-screen", Comment);
+import './reset.css';
+import './comment.css';
+import './audioScreen.css';
+
+customElements.define('comment-list', CommentList);
+customElements.define('button-container', Buttons);
+customElements.define('text-area', TextArea);
+customElements.define('audio-screen', AudioScreen);
+customElements.define('comment-screen', Comment);
