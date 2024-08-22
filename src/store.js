a2d07a915212e257818e0d927c78359c59a77767
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2024-08-22 18:02:51 +0900

    feat: store 개념의 상태추가

--- src/store.js
+++ src/store.js
@@ -1,5 +1,13 @@
 const state = {
   comments: [],
+  audio: new Audio(),
+  play: false,
+  muted: false,
+  imgSrc: '',
+  audioSrc: '',
+  audioTitle: '',
 };
+
+// 컴포넌트 Class가 상태를 구독했다면 상태가 변경됬을 때 구독한 컴포넌트 Class들은 this.render() 실행
 
 export default state;
