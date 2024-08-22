21d4722f00b034d2e2348edfef7c045e652e3ffa
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2024-08-22 18:02:20 +0900

    feat: customElements 추가

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import AudioScreen from './components/AudioScreen.js';
+import AudioScreen from './components/audio/index.js';
 import Comment from './components/comment/index.js';
 import Buttons from './components/comment/Buttons.js';
 import TextArea from './components/comment/TextArea.js';
 import CommentList from './components/comment/CommentList.js';
+import AudioController from './components/audio/AudioController.js';
 
-import './reset.css';
-import './comment.css';
-import './audioScreen.css';
+import './styles/reset.css';
+import './styles/comment.css';
+import './styles/audioScreen.css';
 
-customElements.define('comment-list', CommentList);
-customElements.define('button-container', Buttons);
-customElements.define('text-area', TextArea);
 customElements.define('audio-screen', AudioScreen);
+customElements.define('audio-controller', AudioController);
+
 customElements.define('comment-screen', Comment);
+customElements.define('text-area', TextArea);
+customElements.define('button-container', Buttons);
+customElements.define('comment-list', CommentList);
