c716ec8dd290ce32d62739e0d0cf89989b16a1ca
reignkk1 <ruaals456@naver.com>
2024-08-25 00:50:42 +0900

    webpack env setting plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,30 @@
-const path = require("path");
+const path = require('path');
+
+/*
+dotenv plugin 추가해보기
+*/
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   watch: true,
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(?:js|mjs|cjs)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
-            plugins: ["@babel/plugin-syntax-import-attributes"],
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-syntax-import-attributes'],
           },
         },
       },
