427fb88fcb66a22d85c55758fac807b1300133f1
reignkk1 <ruaals456@naver.com>
2024-08-25 17:46:55 +0900

    feat: 댓글 삭제 fetch 모듈추가

--- src/http.js
+++ src/http.js
@@ -1,5 +1,4 @@
-const API_URL =
-  'https://port-0-comfort-server-m010bhal948caa02.sel4.cloudtype.app';
+const API_URL = 'http://localhost:8080';
 
 /*
 server API_URL = https://port-0-comfort-server-m010bhal948caa02.sel4.cloudtype.app
@@ -26,8 +25,12 @@ export async function fetchGetComments() {
   return data;
 }
 
-export async function fetchDeleteComment(id) {
-  const res = await fetch(`${API_URL}/comment/${id}`, {method: 'DELETE'});
+export async function fetchDeleteComment(id, password) {
+  const res = await fetch(`${API_URL}/comment/${id}`, {
+    method: 'DELETE',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({password}),
+  });
   const data = await res.text();
   return data;
 }
