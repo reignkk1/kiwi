8e6834423aa128d9d5eb690803a0b33a14476f30
reignkk1 <ruaals456@naver.com>
2024-09-11 12:15:53 +0900

    feat: modal PlayList component

--- src/components/home/main/ModalPlayList.js
+++ src/components/home/main/ModalPlayList.js
@@ -1,4 +1,4 @@
-import {audioStore, playListStore} from '../../../store';
+import {audioStore, modalPlayListStore, playListStore} from '../../../store';
 import {playListEvent} from '../events';
 import {getMusicInfo} from '../utils';
 
@@ -12,32 +12,37 @@ class ModalPlayList extends HTMLElement {
   }
 
   render() {
-    this.innerHTML = `
-            <div class='modal-playlist'>
-                <ul>
-                    ${playListStore.music.data
-                      .map(
-                        ({title, imgNumber}) => `
-                    <li class='list-wrap'>
-                        <div>
-                            <img class='list-img' src='./assets/img/${imgNumber}.png' />
-                        </div>
-                        <div>
-                            <div class='list-title'>
-                                <span style='${audioStore.title === title ? 'color: #cd93c9' : ''}'>${getMusicInfo(title).musicTitle}</span>
-                            </div>
-                            <div class='list-singer'>
-                                <span>${getMusicInfo(title).singer}</span>
-                            </div>
-                        </div>
-                    </li>
-                    `
-                      )
-                      .join('')}
-                    
-                </ul>
-            </div>
-            `;
+    if (modalPlayListStore.show) {
+      this.innerHTML = `
+      <div class='modal-playlist'>
+          <ul>
+              ${playListStore.music.data
+                .map(
+                  ({title, imgNumber}) => `
+              <li class='list-wrap'>
+                  <div>
+                      <img class='list-img' src='./assets/img/${imgNumber}.png' />
+                  </div>
+                  <div>
+                      <div class='list-title'>
+                          <span style='${audioStore.title === title ? 'color: #cd93c9' : ''}'>${getMusicInfo(title).musicTitle}</span>
+                      </div>
+                      <div class='list-singer'>
+                          <span>${getMusicInfo(title).singer}</span>
+                      </div>
+                  </div>
+              </li>
+              `
+                )
+                .join('')}
+              
+          </ul>
+      </div>
+      `;
+    } else {
+      this.innerHTML = null;
+    }
+
     this.addEvents();
   }
 
