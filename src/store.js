18975bd9446f5a7b33a784d554ebdeec1e546b07
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2024-09-11 17:59:37 +0900

    feat: musicTitleStore

--- src/store.js
+++ src/store.js
@@ -23,6 +23,23 @@ export const modalPlayListStore = {
   show: false,
 };
 
-// setState 함수 객체를 따로 만들어보기 set함수를 실행하면 자동 렌더링
-export function useStore() {}
+export const musicTitleStore = {
+  animation: false,
+};
+
+const modalMessage = createStore(
+  {
+    show: false,
+    text: '',
+  },
+  ['home-main']
+);
+export function createStore(state, components) {
+  return {};
+}
+
+createStore();
+
 // 컴포넌트 Class가 상태를 구독했다면 상태가 변경됬을 때 구독한 컴포넌트 Class들은 this.render() 실행
+// 인자로 상태와 상태가 변경되면 재렌더링 될 class 컴포넌트를 배열의 형태로 받는다.
+// state 객체를 기반으로 set함수들을 만들고 배열에 담긴 컴포넌트들을 재렌더링 시킨다.
