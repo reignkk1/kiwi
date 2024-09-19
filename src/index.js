cf47510b715031a61c2e0855077cd0faedb68b36
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2024-09-19 17:59:20 +0900

    feat: customElements define 추가 설정

--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
 import Layout from './components/layout';
 import Home from './components/home';
-import Header from './components/home/header';
-import Main from './components/home/main';
-import Footer from './components/home/footer';
 
-import TimeLine from './components/home/footer/TimeLine.js';
-import AudioController from './components/home/footer/AudioController.js';
-import ModalMessage from './components/home/footer/ModalMessage.js';
+import Header from './components/home/header';
+import MusicInfo from './components/home/header/MusicInfo.js';
+import MenuButtons from './components/home/header/MenuButtons.js';
+
+import Main from './components/home/main';
+import MusicImg from './components/home/main/MusicImg.js';
 import ModalPlayList from './components/home/main/ModalPlayList.js';
+
+import Footer from './components/home/footer';
+import TimeLine from './components/home/footer/TimeLine.js';
+import ModalMessage from './components/home/footer/ModalMessage.js';
+import AudioController from './components/home/footer/AudioController.js';
+
+import './styles/home/header.css';
+import './styles/home/main.css';
+import './styles/home/footer.css';
 
 import './styles/reset.css';
 import './styles/layout.css';
-import './styles/home.css';
 
 customElements.define('layout-root', Layout);
 customElements.define('home-content', Home);
 
 customElements.define('home-header', Header);
+customElements.define('music-info', MusicInfo);
+customElements.define('menu-buttons', MenuButtons);
 
 customElements.define('home-main', Main);
+customElements.define('music-img', MusicImg);
 customElements.define('play-list', ModalPlayList);
 
 customElements.define('home-footer', Footer);
