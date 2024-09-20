d108f9eef36eafbe22be311dec3a45ca7cb70cb7
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2024-09-20 18:07:38 +0900

    feat: 현재 재생 중인 곡은 music bar 표시 및 스크롤 위치 이동

--- src/components/home/main/ModalPlayList.js
+++ src/components/home/main/ModalPlayList.js
@@ -1,8 +1,4 @@
-import {
-  modalPlayListStore,
-  musicInfoStore,
-  playListStore,
-} from '../../../store';
+import {musicInfoStore, playListStore} from '../../../store';
 import {playListButtonEvent} from '../events';
 
 class ModalPlayList extends HTMLElement {
@@ -15,22 +11,18 @@ class ModalPlayList extends HTMLElement {
   }
 
   render() {
-    const {getState: getModalPlayList} = modalPlayListStore;
     const {getState: getMusicInfo} = musicInfoStore;
-
     const {title: currentTitle} = getMusicInfo();
-    const {show} = getModalPlayList();
 
-    if (show) {
-      this.innerHTML = `
-      <div class='modal-playlist'>
+    this.innerHTML = `
           <ul>
               ${playListStore.music.data
                 .map(
                   ({title, singer, imgNumber}) => `
               <li class='list-wrap'>
-                  <div>
+                  <div class='list-cover'>
                       <img class='list-img' src='./assets/img/${imgNumber}.png' />
+                      ${currentTitle === title ? `<img class='music-bar' src='./assets/img/music-bar.gif' />` : ''}
                   </div>
                   <div>
                       <div class='list-title'>
@@ -46,17 +38,22 @@ class ModalPlayList extends HTMLElement {
                 .join('')}
               
           </ul>
-      </div>
       `;
-    } else {
-      this.innerHTML = null;
-    }
 
+    this.scrollIntoViewCurrentTitle(currentTitle);
     this.addEvents();
   }
 
   addEvents() {
     playListButtonEvent();
+  }
+
+  scrollIntoViewCurrentTitle(currentTitle) {
+    this.querySelectorAll('.list-title span').forEach((title) => {
+      if (title.innerText === currentTitle) {
+        title.scrollIntoView({behavior: 'smooth', block: 'center'});
+      }
+    });
   }
 }
 
