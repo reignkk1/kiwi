791540d6bf92b46aee4ec0b261083340eed82a00
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2024-09-23 18:21:18 +0900

    feat: 노래 history 모듈 구현 및 팩토리 패턴 적용

--- src/components/home/utils.js
+++ src/components/home/utils.js
@@ -1,71 +1,109 @@
 import {
   audio,
   audioControllerStore,
+  historyMusicStore,
   musicInfoStore,
   playListStore,
 } from '../../store';
 
-export function choiceRandomMusic() {
-  const musicData = playListStore.music.data;
-  const {setState: setMusicInfo} = musicInfoStore;
-  const {title, singer, imgNumber} =
-    musicData[Math.floor(Math.random() * musicData.length)];
+export function handleLocationIndex(action) {
+  const {getState: getHistoryMusic, setState: setHistoryMusic} =
+    historyMusicStore;
 
-  setMusicInfo({
-    title,
-    singer,
-    imgSrc: `./assets/img/${imgNumber}.png`,
-    slide: title.length > 10 ? true : false,
-  });
+  const currentLocationIndex = getHistoryMusic().locationIndex;
+  let newLocationIndex;
 
-  audio.src = `./assets/mp3/${singer + ' - ' + title}.mp3`;
+  switch (action) {
+    case 'prev':
+      if (currentLocationIndex === 0) {
+        newLocationIndex = 0;
+      } else {
+        newLocationIndex = currentLocationIndex - 1;
+      }
+      break;
+    case 'next':
+      newLocationIndex = currentLocationIndex + 1;
+      break;
+  }
+
+  setHistoryMusic({locationIndex: newLocationIndex});
 }
 
-export function choiceNextMusic() {
+export function handleHistory(action) {
+  const {getState: getHistoryMusic, setState: setHistoryMusic} =
+    historyMusicStore;
+  const {getState: getMusicInfo} = musicInfoStore;
+
+  const currentMusic = getMusicInfo();
+  let newHistory = getHistoryMusic().history;
+
+  if (action === 'push') {
+    newHistory.push(currentMusic);
+  } else if (action === 'unshift') {
+    newHistory.unshift(currentMusic);
+  }
+
+  setHistoryMusic({history: newHistory});
+}
+
+export function choiceMusic(action) {
   const musicData = playListStore.music.data;
   const {getState: getMusicInfo, setState: setMusicInfo} = musicInfoStore;
 
+  let newMusicInfo;
   let currentMusicIndex = musicData.findIndex(
-    ({title}) => getMusicInfo().title === title
+    (music) => music.title === getMusicInfo().title
   );
 
-  if (currentMusicIndex === musicData.length - 1) {
-    currentMusicIndex = -1;
+  const createMusicInfo = (musicInfo) => {
+    return {...musicInfo, slide: musicInfo.title.length > 10 ? true : false};
+  };
+
+  switch (action) {
+    case 'random':
+      newMusicInfo = createMusicInfo(
+        musicData[Math.floor(Math.random() * musicData.length)]
+      );
+      break;
+    case 'next':
+      if (currentMusicIndex === musicData.length - 1) {
+        currentMusicIndex = -1;
+      }
+      newMusicInfo = createMusicInfo(musicData[currentMusicIndex + 1]);
+      break;
+    case 'prev':
+      if (currentMusicIndex === 0) {
+        currentMusicIndex = musicData.length - 1;
+      }
+      newMusicInfo = createMusicInfo(musicData[currentMusicIndex - 1]);
+      break;
   }
 
-  const {title, imgNumber, singer} = musicData[currentMusicIndex + 1];
-
-  setMusicInfo({
-    title,
-    singer,
-    imgSrc: `./assets/img/${imgNumber}.png`,
-    slide: title.length > 10 ? true : false,
-  });
-
-  audio.src = `./assets/mp3/${singer + ' - ' + title}.mp3`;
+  audio.src = `./assets/mp3/${newMusicInfo.singer + ' - ' + newMusicInfo.title}.mp3`;
+  setMusicInfo(newMusicInfo);
 }
 
-export function choiceMusic(title, singer, imgNumber) {
+export function choiceSelectMusic(title, singer, imgSrc) {
   const {setState: setMusicInfo} = musicInfoStore;
 
   setMusicInfo({
     title,
     singer,
-    imgSrc: `./assets/img/${imgNumber}.png`,
+    imgSrc,
     slide: title.length > 10 ? true : false,
   });
 
   audio.src = `./assets/mp3/${singer + ' - ' + title}.mp3`;
 }
 
-export function audioPlay() {
+export function handleAudio(action) {
   const {setState: setAudioController} = audioControllerStore;
-  setAudioController({play: true});
-  audio.play();
-}
 
-export function audioPause() {
-  const {setState: setAudioController} = audioControllerStore;
-  setAudioController({play: false});
-  audio.pause();
+  if (action === 'play') {
+    setAudioController({play: true});
+    audio.play();
+  } else if (action === 'pause') {
+    setAudioController({play: false});
+    audio.pause();
+  }
 }
