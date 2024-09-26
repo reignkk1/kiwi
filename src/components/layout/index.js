4ba0dfae92a916b7ed87aeb06665e055a855430c
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2024-09-26 18:22:50 +0900

    feat: BackGround 컴포넌트 추가 및 테두리 edge 추가

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,4 +1,5 @@
 import {audio} from '../../store';
+import {choiceMusic, handleHistory} from '../home/utils';
 
 class Layout extends HTMLElement {
   constructor() {
@@ -6,14 +7,22 @@ class Layout extends HTMLElement {
   }
 
   connectedCallback() {
-    audio.volume = 0.5;
+    // 첫 화면 렌더링 시 음악 랜덤으로 뽑고
+    // 뽑은 음악 history에 넣기
     this.render();
+    choiceMusic('random');
+    handleHistory('push');
+
+    // 노래 초기 볼륨 0.5 설정
+    audio.volume = 0.5;
   }
 
   render() {
     this.innerHTML = `
         <div id='wrap'>
             <div id='root'>
+              <div class='edge'></div>
+              <back-ground></back-ground>
               <home-content></home-content>
             </div>
         </div>
