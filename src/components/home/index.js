03668bd521462f63bfeb5b43d6e21141e25db622
reignkk1 <ruaals456@naver.com>
2024-10-10 16:22:49 +0900

    refactor: 전체적인 구조 변경

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -12,9 +12,20 @@ class Home extends HTMLElement {
   render() {
     this.innerHTML = `
             <div class='screen'>
-                <home-header></home-header>
-                <home-main></home-main>
-                <home-footer></home-footer>
+              <header class='home-header'>
+                <music-info></music-info>
+                <menu-buttons></menu-buttons>
+              </header>
+              <main class='home-main'>
+                <music-img></music-img>
+                <lyrics-wrap></lyrics-wrap>
+                <play-list-wrap></play-list-wrap>
+              </main>
+              <footer class='home-footer'>
+                <time-line></time-line>
+                <modal-message></modal-message>
+                <audio-controller></audio-controller>
+              </footer>
             </div>
             `;
     this.addEvents();
