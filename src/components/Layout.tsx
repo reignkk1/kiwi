b9131897e6fd4880e2765a571978144901c100be
reignkk1 <ruaals456@naver.com>
2024-11-07 17:04:33 +0900

    feat: Layout header, footer 구현

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,33 +1,44 @@
 import { ReactNode } from "react";
 import styled from "styled-components";
+import { Footer } from "./layout/Footer";
+import Header from "./layout/Header";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <Layout.Container>
+    <Container>
       <BackGroundFilter />
-      <Edge>{children}</Edge>
-    </Layout.Container>
-  );
-}
-
-function Edge({ children }: { children: ReactNode }) {
-  return (
-    <Edge.Container>
-      <Content>{children}</Content>
-    </Edge.Container>
+      <Edge>
+        <Content>
+          <Header title="민겸님 어서오세요!" />
+          {children}
+        </Content>
+        <Footer />
+      </Edge>
+    </Container>
   );
 }
 
 const Content = styled.div`
   height: 100%;
-  padding: 60px 40px 40px 40px;
+  margin-top: 100px;
+  overflow: auto;
+  padding: 0px 50px 0px 50px;
+  & ::-webkit-scrollbar {
+    background-color: black;
+    height: 3px;
+    cursor: pointer;
+  }
+  & ::-webkit-scrollbar-thumb {
+    background-color: grey;
+    cursor: pointer;
+  }
 `;
 
 const BackGroundFilter = styled.div`
   width: 430px;
   height: 785px;
   border-radius: 40px;
-  background-color: red;
+  background-color: rgba(0, 0, 0, 0.8);
   position: absolute;
   z-index: 1;
   @media only screen and (max-height: 825px) {
@@ -35,25 +46,15 @@ const BackGroundFilter = styled.div`
   }
 `;
 
-Layout.Container = styled.div`
-  width: 100%;
-  height: 100vh;
-  background: linear-gradient(-45deg, #fff6b7, #f092bc, #8795e6, #c2ffd8);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  user-select: none;
-  @media only screen and (min-device-width: 360px) and (max-device-width: 479px) {
-    height: calc(var(--vh, 1vh) * 100);
-  }
-`;
-
-Edge.Container = styled.div`
+const Edge = styled.div`
   width: 460px;
   height: 830px;
   background: url("./img/phone.png") no-repeat;
   position: relative;
   z-index: 2;
+  & ::-webkit-scrollbar {
+    width: 0px;
+  }
 
   @media only screen and (min-device-width: 360px) and (max-device-width: 479px) {
     width: 100%;
@@ -66,5 +67,19 @@ Edge.Container = styled.div`
 
   @media only screen and (min-device-width: 360px) and (max-device-width: 479px) {
     display: none;
+  }
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  background: linear-gradient(-45deg, #fff6b7, #f092bc, #8795e6, #c2ffd8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  user-select: none;
+  @media only screen and (min-device-width: 360px) and (max-device-width: 479px) {
+    height: calc(var(--vh, 1vh) * 100);
   }
 `;
