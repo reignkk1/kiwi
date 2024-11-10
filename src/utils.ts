3972d482ff926e47d8ea8b9ec70455b4f5fdcbc3
reignkk1 <ruaals456@naver.com>
2024-11-10 23:01:31 +0900

    feat: useActiveSection Module

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,11 @@
-import pageConfig from "./pageConfig.json";
+import { useLocation } from "react-router-dom";
 
-export function getPageConfig(pathname: string) {
+export function useActiveSection() {
+  const { pathname } = useLocation();
+
   if (pathname === "/") {
-    return pageConfig["home"];
+    return "home";
+  } else {
+    return pathname.split("/")[1];
   }
 }
-
-// 페이지가 홈 '/' 일때 getPageConfig는 homeConfig 타입을 리턴해주고
-// 다른 페이지일 때는 그거에 맞는 config 타입을 리턴 해주게끔.
-
-// 결론: 페이지 pathname 하나하나 설정해주자 타입을.
