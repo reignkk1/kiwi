32c707b4148aa0e35cea5b85b1e088fdc841a5a2
reignkk1 <ruaals456@naver.com>
2024-11-10 23:05:21 +0900

    style: button Global Style

--- src/components/GlobalStyles.tsx
+++ src/components/GlobalStyles.tsx
@@ -3,7 +3,17 @@ import reset from "styled-reset";
 
 export const GlobalStyles = createGlobalStyle`
     ${reset}
-    *{
+    :root{
+        --signature-color:#0ceb0c;
+    }
+
+    * {
         box-sizing:border-box;
+    }
+
+    button {
+        background:none;
+        border:none;
+        cursor: pointer;
     }
 `;
