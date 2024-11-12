b40c0a22bfcac561307f57bc5ae417e7d77d12fb
reignkk1 <ruaals456@naver.com>
2024-11-12 23:50:17 +0900

    feat: 글자중에 키워드에 해당하는 부분은 mark 태그로 감싸는 함수 추가

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom";
+import parse from "html-react-parser";
 
 export function useActiveSection() {
   const { pathname } = useLocation();
@@ -8,4 +9,8 @@ export function useActiveSection() {
   } else {
     return pathname.split("/")[1];
   }
+}
+
+export function markKeyword(letter: string, keyWord: string) {
+  return parse(letter.replaceAll(keyWord, `<mark>${keyWord}</mark>`));
 }
