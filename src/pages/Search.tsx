c405f24fd0b8b9d60e5424e2616fbc2d01809f08
reignkk1 <ruaals456@naver.com>
2024-11-12 23:52:49 +0900

    feat: SearchBar, SearchList 컴포넌트 분리

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,9 +1,11 @@
 import SearchBar from "../components/pages/search/SearchBar";
+import SearchList from "../components/pages/search/SearchList";
 
 export default function Search() {
   return (
     <>
       <SearchBar />
+      <SearchList />
     </>
   );
 }
