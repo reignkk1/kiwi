3d5c9188d676568f7a1a6637066cd41e2d923e33
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-02-07 18:26:22 +0900

    refactor: TitleAndSInger 컴포넌트 import

--- src/components/pages/home/Album.tsx
+++ src/components/pages/home/Album.tsx
@@ -1,39 +1,25 @@
 import styled from "styled-components";
 import AlbumImg from "../../shared/AlbumImg";
 import { MusicType } from "../../shared/types";
+import { TitleAndSinger } from "../../shared/TitleAndSinger";
 
 interface AlbumProps {
   musicInfo: MusicType;
 }
 
 export default function Album({ musicInfo }: AlbumProps) {
+  const { title, singer } = musicInfo;
   return (
     <AlbumTemplate>
       <AlbumImg type="middle" musicInfo={musicInfo} isActiveButton={true} />
-      <AlbumInfo>
-        <Title>{musicInfo.title}</Title>
-        <Singer>{musicInfo.singer}</Singer>
-      </AlbumInfo>
+      <TitleAndSinger title={title} singer={singer} size="small" />
     </AlbumTemplate>
   );
 }
 
 const AlbumTemplate = styled.div`
   width: 130px;
-`;
-
-const AlbumInfo = styled.div`
-  margin-top: 10px;
-  font-size: 14px;
-`;
-const Title = styled.div`
-  color: white;
-  font-weight: bold;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  margin-bottom: 5px;
-`;
-const Singer = styled.div`
-  color: var(--singer-color);
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 `;
