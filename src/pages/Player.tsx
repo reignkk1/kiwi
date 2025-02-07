ebdbfcd83ef76b56dc1dfc8dbc2a7b6779faf504
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-02-07 18:30:01 +0900

    feat: 메뉴 상태에 따른 조건문 컴포넌트 렌더링

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -3,9 +3,15 @@ import ControllerButtons from "../components/pages/player/ControllerButtons";
 import LyricsAndImage from "../components/pages/player/LyricsAndImage";
 import PlayerHeader from "../components/pages/player/PlayerHeader";
 import { ProgressBar } from "../components/shared/ProgressBar";
+import { createIsPlayerMenuStore } from "../components/pages/player/store";
+import PlayerMenu from "../components/pages/player/PlayerMenu";
 
 export default function Player() {
-  return (
+  const isPlayerMenu = createIsPlayerMenuStore((state) => state.isPlayerMenu);
+
+  return isPlayerMenu ? (
+    <PlayerMenu />
+  ) : (
     <div>
       <PlayerHeader />
       <LyricsAndImage />
