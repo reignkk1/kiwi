fdf63ee3757a29b268dd0ba13b36cc52c046485b
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-03-12 18:10:00 +0900

    feat: toggleFadeAlertMessage Action 추가

--- src/components/pages/player/ControllerButtons.tsx
+++ src/components/pages/player/ControllerButtons.tsx
@@ -1,14 +1,42 @@
-import { faShuffle } from "@fortawesome/free-solid-svg-icons";
+import { faRepeat, faShuffle } from "@fortawesome/free-solid-svg-icons";
 import { ButtonIcon } from "../../shared/ButtonIcon";
 import Controller from "../../shared/Controller";
 import styled from "styled-components";
+import { useControllerButtonsStore } from "./hooks";
 
 export default function ControllerButtons() {
+  const {
+    state: { isRepeat, isShuffle },
+    action: { toggleRepeat, toggleShuffle, toggleFadeAlertMessage },
+  } = useControllerButtonsStore();
+
   return (
     <Container>
-      <ButtonIcon icon={faShuffle} />
+      <ButtonIcon
+        active={isShuffle}
+        icon={faShuffle}
+        onClick={() => {
+          toggleShuffle();
+          if (isShuffle) {
+            toggleFadeAlertMessage("셔플을 사용하지 않습니다.");
+          } else {
+            toggleFadeAlertMessage("셔플을 사용합니다.");
+          }
+        }}
+      />
       <Controller width={170} size={[30, 40, 30]} />
-      <ButtonIcon icon={faShuffle} />
+      <ButtonIcon
+        active={isRepeat}
+        icon={faRepeat}
+        onClick={() => {
+          toggleRepeat();
+          if (isRepeat) {
+            toggleFadeAlertMessage("반복을 사용하지 않습니다.");
+          } else {
+            toggleFadeAlertMessage("현재 음악을 반복합니다.");
+          }
+        }}
+      />
     </Container>
   );
 }
