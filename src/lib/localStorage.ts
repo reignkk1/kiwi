8f4a9071a30b0502d524f487a94f78e845696610
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-03-14 18:33:05 +0900

    feat: localStorage lib 구현체 개발

--- src/lib/localStorage.ts
+++ src/lib/localStorage.ts
@@ -1,1 +1,49 @@
-export const localStorages = localStorage;
+type Key = string;
+type Value = string | number | (string | number | object)[];
+
+type ValueObject = Record<Key, Value>;
+
+type MusicListStorage = {
+  age: number;
+};
+
+export const musicListStorage = createLocalStorageImpl<MusicListStorage>({
+  age: 27,
+});
+
+function createLocalStorageImpl<T extends ValueObject>(object: T) {
+  const keys = Object.keys(object);
+  const values = Object.values(object);
+
+  Object.entries(object).forEach(([key, value]) => {
+    let stringifyValue;
+
+    if (typeof value === "number" || typeof value === "object") {
+      stringifyValue = JSON.stringify(value);
+    } else {
+      stringifyValue = value;
+    }
+
+    localStorage.setItem(key, stringifyValue);
+  });
+
+  const set = (newObject: T) => {
+    Object.entries(newObject).forEach(([key, value]) => {
+      let stringifyValue;
+
+      if (typeof value === "number" || typeof value === "object") {
+        stringifyValue = JSON.stringify(value);
+      } else {
+        stringifyValue = value;
+      }
+
+      localStorage.setItem(key, stringifyValue);
+    });
+  };
+
+  const get = (key: keyof T) => {
+    return localStorage.getItem(key as string);
+  };
+
+  return { set, get };
+}
