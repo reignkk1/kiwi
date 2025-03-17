b6d8f1a6e242f37a79679ff2ba389b94df44eff1
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-03-17 17:57:22 +0900

    refactor: utils 함수들의 기능 설명 작성

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 import parse from "html-react-parser";
 import { useLocation } from "react-router-dom";
 
+// 현재 어떤 페이지에 위치하고 있는지 리턴해주는 함수
 export function useCurrentPage() {
   const { pathname } = useLocation();
 
@@ -14,14 +15,25 @@ export function useCurrentPage() {
   return pageMap[pathname] || "unknown";
 }
 
+// letter 인자로 들어온 글자들 중 keyWord에 해당하는 부분을 marking 해주는 함수
 export function markKeyword(letter: string, keyWord: string) {
   return parse(letter.replaceAll(keyWord, `<mark>${keyWord}</mark>`));
 }
 
+// 총 길이에서 현재 시간을 인자 값으로 받아 퍼센트로 나타내주는 함수
+// ex) getProgressPercent(50,100) => 50%
 export function getProgressPercent(currentTime: number, duration: number) {
   return Number(((currentTime / duration) * 100).toFixed(1));
 }
 
+// 총 길이와 퍼센트 숫자를 인자 값으로 넣으면 현재 시간이 리턴된다.
+export function convertFromPercentToTime(duration: number, percent: number) {
+  return Math.floor(Math.floor(duration) * (percent / 100));
+}
+
+// 숫자로 된 초시간을 인자 값으로 받아 쌍점(:)을 사용한 표기법으로 변환
+// 1분 10초 => 70초
+// ex) convertTime(70) => 1:10
 export function convertTime(time: number) {
   time = Math.floor(time);
 
@@ -31,10 +43,7 @@ export function convertTime(time: number) {
   return `${minute}:${second}`;
 }
 
-export function convertFromPercentToTime(duration: number, percent: number) {
-  return Math.floor(Math.floor(duration) * (percent / 100));
-}
-
+// 타입 확인 is 객체 => boolean 값 리턴
 export const is = {
   boolean: (data: any) => typeof data === "boolean",
   number: (data: any) => typeof data === "number",
@@ -42,6 +51,7 @@ export const is = {
   string: (data: any) => typeof data === "string",
 };
 
+// 어레이 안에 있는 값들을 랜덤으로 뽑는 함수
 export function selectRandomWithinArray(array: Array<string | number>) {
   return array[Math.floor(Math.random() * array.length)];
 }
