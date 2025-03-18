6de522a912b1a9e6107a95e9a026d8cccd40ce51
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-03-18 18:34:16 +0900

    feat: Audio 컴포넌트 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GlobalStyles } from "./components/GlobalStyles";
+import Audio from "./components/Audio";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <>
+    <Audio />
     <GlobalStyles />
     <App />
   </>
