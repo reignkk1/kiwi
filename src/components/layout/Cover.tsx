0691d5c1eb6780e88db55e7a0f85510113694cc8
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-05-07 18:26:04 +0900

    updates

--- src/components/layout/Cover.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-import styled from "styled-components";
-
-export default function Cover({ children }: { children: ReactNode }) {
-  return <Container>{children}</Container>;
-}
-
-const Container = styled.div``;
