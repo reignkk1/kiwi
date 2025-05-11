6f0b79ada0b5e7ffc7a353ddd51f2bd10f7272c0
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-05-12 00:38:46 +0900

    feat: 페이지들 타입 추가

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,8 @@
-export interface MusicType {
+export type GenreMenuType = "all" | "ballad" | "indie" | "hiphop";
+
+export type Pages = "home" | "search" | "storage" | "player" | "music";
+
+export type MusicType = {
   id: number;
   title: string;
   singer: string;
@@ -6,6 +10,4 @@ export interface MusicType {
   backGroundColor: Array<string>;
   genre: string;
   lyrics: Array<{ text: string; startTime: number; endTime: number }>;
-}
-
-export type GenreMenuType = "all" | "ballad" | "indie" | "hiphop";
+};
