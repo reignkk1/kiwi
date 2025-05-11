abdeb19474b53d4bc2edc85ad19b97ade28f6311
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-05-12 00:35:55 +0900

    feat: 뮤직 페이지에서 음악 정보 컴포넌트

--- src/components/pages/music/MusicInfo.tsx
+++ src/components/pages/music/MusicInfo.tsx
@@ -1,7 +1,39 @@
 import styled from "styled-components";
+import useGetMusicInfoById from "../../../hooks/useGetMusicInfoById";
+import AlbumImg from "../../shared/AlbumImg";
+import { TitleAndSinger } from "../../shared/TitleAndSinger";
 
 export default function MusicInfo() {
-  return <Container></Container>;
+  const musicInfo = useGetMusicInfoById()!;
+
+  return (
+    <Container>
+      <div>
+        <AlbumTitle>
+          <span>{musicInfo.albumTitle}</span>
+        </AlbumTitle>
+        <TitleAndSinger
+          width="200px"
+          title={musicInfo.title}
+          singer={musicInfo.singer}
+          size="large"
+        />
+      </div>
+      <AlbumImg musicInfo={musicInfo} type="middle" />
+    </Container>
+  );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 50px;
+`;
+
+const AlbumTitle = styled.div`
+  span {
+    color: rgba(255, 255, 255, 0.7);
+  }
+  margin-bottom: 15px;
+`;
