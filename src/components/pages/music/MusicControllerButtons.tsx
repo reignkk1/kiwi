07be0ed5c94bb41fdbb003a2eee1f01249e6fa8b
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-05-13 00:20:15 +0900

    feat: music controller buttons 컴포넌트 구현

--- src/components/pages/music/MusicControllerButtons.tsx
+++ src/components/pages/music/MusicControllerButtons.tsx
@@ -1,7 +1,48 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import styled from "styled-components";
+import { ButtonIcon } from "../../shared/ButtonIcon";
+import { palette } from "../../../constant";
+import { faPlay, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 export default function MusicControllerButtons() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <ControllerButton icon={faPlay} text="재생" />
+      <ControllerButton icon={faPlus} text="담기" />
+    </Container>
+  );
 }
 
-const Container = styled.div``;
+function ControllerButton({ icon, text }: { icon: IconProp; text: string }) {
+  return (
+    <ButtonContainer>
+      <ButtonIcon icon={icon} />
+      <Text>
+        <span>{text}</span>
+      </Text>
+    </ButtonContainer>
+  );
+}
+const Container = styled.div`
+  margin-top: 15px;
+  width: 100%;
+  display: flex;
+  gap: 10px;
+`;
+
+const ButtonContainer = styled.button`
+  width: 50%;
+  background-color: ${palette.textButton};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  padding: 10px 0px;
+`;
+
+const Text = styled.div`
+  color: white;
+  margin-left: 5px;
+  font-size: 16px;
+  margin-bottom: 3px;
+`;
