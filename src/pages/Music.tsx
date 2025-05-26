30e1380da564527ac61fc170e552e0c053230e70
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-05-26 15:49:28 +0900

    feat: 스크롤 이벤트와 url 파라미터 id값이 변경될 때마다 musicData 가져오기

--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -3,14 +3,30 @@ import MusicInfo from "../components/pages/music/MusicInfo";
 import MusicControllerButtons from "../components/pages/music/MusicControllerButtons";
 import MusicHeader from "../components/pages/music/MusicHeader";
 import MusicLyrics from "../components/pages/music/MusicLyrics";
+import { useMusicScrollStore } from "../store/music/useMusicScrollStore";
+import { useMusicDataFromId } from "../store/music/useMusicDataFromId";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import MusicCredit from "../components/pages/music/MusicCredit";
 
 export default function Music() {
+  const { id } = useParams();
+  const setScrollTop = useMusicScrollStore((state) => state.setScrollTop);
+  const getMusicDataFromId = useMusicDataFromId(
+    (state) => state.getMusicDataFromId
+  );
+
+  useEffect(() => {
+    if (id) getMusicDataFromId(id);
+  }, [id]);
+
   return (
-    <Container>
+    <Container onScroll={(e) => setScrollTop(e.currentTarget.scrollTop)}>
       <MusicHeader />
       <MusicInfo />
       <MusicControllerButtons />
       <MusicLyrics />
+      <MusicCredit />
     </Container>
   );
 }
