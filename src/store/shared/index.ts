7faa5f53bf552885ceeeb4e81d91974034026385
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-05-27 19:04:19 +0900

    refactor: scroll store 리팩토링

--- src/store/shared/index.ts
+++ src/store/shared/index.ts
@@ -2,10 +2,12 @@ import { useAlertStore } from "./useAlertStore";
 import { useCurrentMusicStore } from "./useCurrentMusicStore";
 import { usePlayDirectionStore } from "./usePlayDirectionStore";
 import { useProgressStore } from "./useProgressStore";
+import { useScrollStore } from "./useScrollStore";
 
 export {
   useAlertStore,
   useCurrentMusicStore,
   usePlayDirectionStore,
   useProgressStore,
+  useScrollStore,
 };
