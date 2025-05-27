767f63e907a3e971e2b05b339ae06126405669c5
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-05-27 19:03:57 +0900

    feat: scrollTitleLayout 컴포넌트 추가

--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -1,40 +1,30 @@
-import styled from "styled-components";
 import MusicInfo from "../components/pages/music/MusicInfo";
 import MusicControllerButtons from "../components/pages/music/MusicControllerButtons";
-import MusicHeader from "../components/pages/music/MusicHeader";
 import MusicLyrics from "../components/pages/music/MusicLyrics";
-import { useMusicScrollStore } from "../store/music/useMusicScrollStore";
 import { useMusicDataFromId } from "../store/music/useMusicDataFromId";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import MusicCredit from "../components/pages/music/MusicCredit";
+import ScrollTitleLayout from "../components/layout/ScrollTitleLayout";
 
 export default function Music() {
   const { id } = useParams();
-  const setScrollTop = useMusicScrollStore((state) => state.setScrollTop);
+
   const getMusicDataFromId = useMusicDataFromId(
     (state) => state.getMusicDataFromId
   );
+  const music = useMusicDataFromId((state) => state.music);
 
   useEffect(() => {
     if (id) getMusicDataFromId(id);
   }, [id]);
 
   return (
-    <Container onScroll={(e) => setScrollTop(e.currentTarget.scrollTop)}>
-      <MusicHeader />
+    <ScrollTitleLayout title={music?.title}>
       <MusicInfo />
       <MusicControllerButtons />
       <MusicLyrics />
       <MusicCredit />
-    </Container>
+    </ScrollTitleLayout>
   );
 }
-
-const Container = styled.div`
-  height: 100%;
-  overflow: auto;
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`;
