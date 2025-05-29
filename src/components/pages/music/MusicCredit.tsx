f8cdf895aefbc4033cdc60b407129af363079ade
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-05-29 16:40:52 +0900

    feat: infoMap 객체를 InfoTable 컴포넌트에 주입

--- src/components/pages/music/MusicCredit.tsx
+++ src/components/pages/music/MusicCredit.tsx
@@ -1,15 +1,16 @@
 import styled from "styled-components";
 import { useMusicDataFromId } from "../../../store/music/useMusicDataFromId";
 import { convertToGenreKorea } from "./../../../utils";
+import InfoTable from "../../shared/InfoTable";
 
 export default function MusicCredit() {
   const music = useMusicDataFromId((state) => state.music);
 
-  const InfoMap = [
-    { title: "작사", value: music?.lyricist },
-    { title: "작곡", value: music?.composer },
-    { title: "편곡", value: music?.arranger },
-    { title: "장르", value: convertToGenreKorea(music?.genre) },
+  const infoMap = [
+    { key: "작사", value: music?.lyricist },
+    { key: "작곡", value: music?.composer },
+    { key: "편곡", value: music?.arranger },
+    { key: "장르", value: convertToGenreKorea(music?.genre) },
   ];
 
   return (
@@ -20,59 +21,23 @@ export default function MusicCredit() {
       <MusicTitle>
         <span>{music?.title}</span>
       </MusicTitle>
-      <Info>
-        {InfoMap.map((info) => (
-          <InfoList key={info.title} title={info.title} value={info.value} />
-        ))}
-      </Info>
+      <InfoTable info={infoMap} />
     </Container>
-  );
-}
-
-function InfoList({ title, value }: { title?: string; value?: string | null }) {
-  return (
-    <List>
-      <ListTitle>
-        <span>{title}</span>
-      </ListTitle>
-      <ListValue>
-        <span>{value}</span>
-      </ListValue>
-    </List>
   );
 }
 
 const Container = styled.div`
   color: white;
-  margin-bottom: 150px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 `;
 
 const Title = styled.h2`
   font-size: 20px;
   font-weight: bold;
-  margin-bottom: 20px;
 `;
 
 const MusicTitle = styled.div`
-  font-size: 14px;
-`;
-
-const Info = styled.ul`
-  margin-top: 30px;
-`;
-
-const List = styled.li`
-  display: flex;
-  align-items: center;
-  gap: 20px;
-  margin-bottom: 20px;
-`;
-
-const ListTitle = styled.div`
-  color: rgba(255, 255, 255, 0.7);
-`;
-const ListValue = styled.div`
-  border-radius: 20px;
-  border: 1px solid rgba(255, 255, 255, 0.2);
-  padding: 10px;
+  font-size: 15px;
 `;
