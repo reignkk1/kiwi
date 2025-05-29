8ebbe76afda0c60c111bfa124350686e6ddfc6a9
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-05-29 16:44:25 +0900

    feat: MusicInfo 컴포넌트 구현

--- src/components/pages/music/MusicInfo.tsx
+++ src/components/pages/music/MusicInfo.tsx
@@ -29,7 +29,6 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding-top: 50px;
 `;
 
 const AlbumTitle = styled.div`
@@ -38,11 +37,3 @@ const AlbumTitle = styled.div`
   }
   margin-bottom: 15px;
 `;
-
-// hooks 폴더에서 페이지 별로 나눠야 할듯? 그게 더 관리하기 쉬움
-// store 폴더에서 나눈것 처럼
-
-// 다른 노래들 작사,작곡, 앨범 타이틀 등 데이터 입력하기
-
-// 색깔 팔레트 상수로 빼기?
-// 인포는 완료 커밋
