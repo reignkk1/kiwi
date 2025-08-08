4cf20a12f811cf16e77d89be4b65e1e406e9835c
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-08-08 17:09:04 +0900

    feat: hover 상태에 따라서 볼륨바 토글기능 및 setTimeout 활용한 자연스러운 토글

--- src/components/pages/player/PlayerVolume.tsx
+++ src/components/pages/player/PlayerVolume.tsx
@@ -3,19 +3,47 @@ import { ButtonIcon } from "../../shared/ButtonIcon";
 import styled from "styled-components";
 import { useMutedStore } from "../../../store/player/useMutedStore";
 import { useShallow } from "zustand/react/shallow";
+import { useRef, useState } from "react";
 
 export default function PlayerVolume() {
   const [isMuted, toggleMuted] = useMutedStore(
     useShallow((state) => [state.isMuted, state.toggleMuted])
   );
 
+  const [isHover, setIsHover] = useState(false);
+
+  const timeOutId = useRef<null | ReturnType<typeof setTimeout>>(null);
+
+  const clearTimer = () => {
+    if (timeOutId.current) {
+      clearTimeout(timeOutId.current);
+      timeOutId.current = null;
+    }
+  };
+
+  const startHideTimer = () => {
+    clearTimer();
+    timeOutId.current = setTimeout(() => setIsHover(false), 2000);
+  };
+
+  const showVolumeBar = () => {
+    clearTimer();
+    setIsHover(true);
+  };
+
   return (
     <Container>
+      {isHover && (
+        <Input
+          type="range"
+          onMouseEnter={showVolumeBar}
+          onMouseLeave={startHideTimer}
+        />
+      )}
       <ButtonIcon
-        onClick={() => {
-          console.log(isMuted);
-          toggleMuted();
-        }}
+        onClick={() => toggleMuted()}
+        onMouseEnter={showVolumeBar}
+        onMouseLeave={startHideTimer}
         ariaLabel="볼륨조절"
         icon={isMuted ? faVolumeMute : faVolumeLow}
       />
@@ -26,4 +54,13 @@ export default function PlayerVolume() {
 const Container = styled.div`
   display: flex;
   justify-content: flex-end;
+  position: relative;
+`;
+
+const Input = styled.input`
+  width: 80px;
+  transform: rotate(270deg);
+  position: absolute;
+  right: -28px;
+  bottom: 60px;
 `;
