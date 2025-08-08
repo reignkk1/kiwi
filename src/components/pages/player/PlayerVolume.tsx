e39060b40edc9d109fa6d783c7093dc4af052d3d
reignkk1 <ruaals456@naver.com>
2025-08-08 12:16:18 +0900

    feat: Muted Icon Button onClikc 이벤트 추가

--- src/components/pages/player/PlayerVolume.tsx
+++ src/components/pages/player/PlayerVolume.tsx
@@ -1,11 +1,24 @@
-import { faVolumeLow } from "@fortawesome/free-solid-svg-icons";
+import { faVolumeLow, faVolumeMute } from "@fortawesome/free-solid-svg-icons";
 import { ButtonIcon } from "../../shared/ButtonIcon";
 import styled from "styled-components";
+import { useMutedStore } from "../../../store/player/useMutedStore";
+import { useShallow } from "zustand/react/shallow";
 
 export default function PlayerVolume() {
+  const [isMuted, toggleMuted] = useMutedStore(
+    useShallow((state) => [state.isMuted, state.toggleMuted])
+  );
+
   return (
     <Container>
-      <ButtonIcon ariaLabel="볼륨조절" icon={faVolumeLow} />
+      <ButtonIcon
+        onClick={() => {
+          console.log(isMuted);
+          toggleMuted();
+        }}
+        ariaLabel="볼륨조절"
+        icon={isMuted ? faVolumeMute : faVolumeLow}
+      />
     </Container>
   );
 }
