a86ba3508f2606c4cde4bdd958a50979f3ea8310
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-08-12 18:37:51 +0900

    feat: 볼륨 애니메이션 show, hide 구현 및 isMuted 상태와 볼륨값에 따른 아이콘 변경

--- src/components/pages/player/PlayerVolume.tsx
+++ src/components/pages/player/PlayerVolume.tsx
@@ -3,52 +3,84 @@ import { ButtonIcon } from "../../shared/ButtonIcon";
 import styled from "styled-components";
 import { useMutedStore } from "../../../store/player/useMutedStore";
 import { useShallow } from "zustand/react/shallow";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useVolumeStore } from "../../../store/player/useVolumeStore";
 
 export default function PlayerVolume() {
-  const [isMuted, toggleMuted] = useMutedStore(
-    useShallow((state) => [state.isMuted, state.toggleMuted])
+  const [isMuted, setIsMuted, toggleMuted] = useMutedStore(
+    useShallow((state) => [state.isMuted, state.setIsMuted, state.toggleMuted])
   );
 
   const [volume, setVolume] = useVolumeStore(
     useShallow((state) => [state.volume, state.setVolume])
   );
 
-  const [isHover, setIsHover] = useState(false);
+  const [isShowVolumeBar, setIsShowVolumeBar] = useState(false);
+  const [hideAnimation, setHideAnimation] = useState(false);
 
-  const timeOutId = useRef<null | ReturnType<typeof setTimeout>>(null);
+  const timerIds = useRef<{
+    showVolumeBar?: ReturnType<typeof setTimeout>;
+    hideAnimation?: ReturnType<typeof setTimeout>;
+  }>({});
 
-  const clearTimer = () => {
-    if (timeOutId.current) {
-      clearTimeout(timeOutId.current);
-      timeOutId.current = null;
+  const prevVolume = useRef<number>(0);
+
+  const clearTimers = () => {
+    if (timerIds.current.hideAnimation && timerIds.current.showVolumeBar) {
+      Object.values(timerIds.current).forEach((timerId) =>
+        clearTimeout(timerId)
+      );
+      timerIds.current = {};
     }
   };
 
   const startHideTimer = () => {
-    clearTimer();
-    timeOutId.current = setTimeout(() => setIsHover(false), 2000);
+    clearTimers();
+    timerIds.current.hideAnimation = setTimeout(
+      () => setHideAnimation(true),
+      1500
+    );
+    timerIds.current.showVolumeBar = setTimeout(
+      () => setIsShowVolumeBar(false),
+      1800
+    );
   };
 
+  useEffect(() => {
+    setIsMuted(volume === 0);
+  }, [isMuted, volume]);
+
   const showVolumeBar = () => {
-    clearTimer();
-    setIsHover(true);
+    clearTimers();
+    setHideAnimation(false);
+    setIsShowVolumeBar(true);
   };
 
   return (
     <Container>
-      <ButtonIcon
-        onClick={() => toggleMuted()}
-        onMouseEnter={showVolumeBar}
-        onMouseLeave={startHideTimer}
-        ariaLabel="볼륨조절"
-        icon={isMuted ? faVolumeMute : faVolumeLow}
-      />
-      {isHover && (
+      <ButtonWrapper>
+        <ButtonIcon
+          onClick={() => {
+            toggleMuted();
+            if (volume > 0) {
+              prevVolume.current = volume;
+              setVolume(0);
+            } else {
+              setVolume(prevVolume.current);
+            }
+            console.log(isMuted);
+          }}
+          onMouseEnter={showVolumeBar}
+          onMouseLeave={startHideTimer}
+          ariaLabel="볼륨조절"
+          icon={isMuted ? faVolumeMute : faVolumeLow}
+        />
+      </ButtonWrapper>
+      {isShowVolumeBar && (
         <Input
           type="range"
           value={volume}
+          hideAnimation={hideAnimation}
           onChange={(e) => setVolume(Number(e.currentTarget.value))}
           onMouseEnter={showVolumeBar}
           onMouseLeave={startHideTimer}
@@ -60,26 +92,34 @@ export default function PlayerVolume() {
 
 const Container = styled.div`
   display: flex;
-  justify-content: space-between;
   align-items: center;
-  width: 120px;
+  gap: 5px;
+  width: 125px;
   padding-left: 2px;
   @media only screen and (min-device-width: 360px) and (max-device-width: 479px) {
     display: none;
   }
 `;
 
-const Input = styled.input.attrs<{ value: number }>(({ value }) => ({
-  style: {
-    background: `linear-gradient(to right, white ${
-      (value / 100) * 100
-    }%, #2A2A2A ${(value / 100) * 100}%)`,
-  },
-}))`
-  width: 80px;
+const ButtonWrapper = styled.div`
+  width: 30px;
+`;
+
+const Input = styled.input.attrs<{ value: number; hideAnimation: boolean }>(
+  ({ value }) => ({
+    style: {
+      background: `linear-gradient(to right, white ${
+        (value / 100) * 100
+      }%, #2A2A2A ${(value / 100) * 100}%)`,
+    },
+  })
+)`
   height: 4px;
   appearance: none;
   cursor: pointer;
+  animation: ${({ hideAnimation }) =>
+      hideAnimation ? "hideVolumeBar" : "showVolumeBar"}
+    0.5s forwards;
 
   &::-webkit-slider-thumb {
     -webkit-appearance: none;
@@ -87,5 +127,24 @@ const Input = styled.input.attrs<{ value: number }>(({ value }) => ({
     height: 15px;
     border-radius: 50%;
     background: white;
+  }
+
+  @keyframes showVolumeBar {
+    0% {
+      width: 0px;
+    }
+
+    100% {
+      width: 80px;
+    }
+  }
+
+  @keyframes hideVolumeBar {
+    0% {
+      width: 80px;
+    }
+    100% {
+      width: 0px;
+    }
   }
 `;
