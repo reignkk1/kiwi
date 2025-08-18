a5db95b3c2c279be6ae303314f9efd5c57e39f81
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-08-18 18:04:59 +0900

    refactor: title 속성 추가

--- src/components/pages/player/PlayerControllerButtons.tsx
+++ src/components/pages/player/PlayerControllerButtons.tsx
@@ -12,6 +12,7 @@ export default function PlayerControllerButtons() {
     <Container>
       <ButtonIcon
         ariaLabel="셔플"
+        title={isShuffle ? "랜덤재생 해제" : "랜덤재생"}
         active={isShuffle}
         icon={faShuffle}
         onClick={handleShuffleClick}
@@ -19,6 +20,7 @@ export default function PlayerControllerButtons() {
       <Controller width={170} size={[30, 40, 30]} />
       <ButtonIcon
         ariaLabel="반복"
+        title={isLoop ? "반복재생 해제" : "반복재생"}
         active={isLoop}
         icon={faRepeat}
         onClick={handleRepeatClick}
