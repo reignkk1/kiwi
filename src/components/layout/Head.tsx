84b392a9e287c92f8943f9552168d381323c3191
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-08-19 16:06:26 +0900

    feat: 현재 페이지에 따라서 head 태그 안에 있는 title이 동적으로 변경

--- src/components/layout/Head.tsx
+++ src/components/layout/Head.tsx
@@ -1,11 +1,26 @@
 import { Helmet } from "react-helmet";
+import { useCurrentPage } from "../../hooks/useCurrentPage";
+import { useCurrentMusicStore } from "../../store/shared";
+import type { Pages } from "../../types";
 
 export default function Head() {
+  const currentPage = useCurrentPage();
+  const currentMusic = useCurrentMusicStore((state) => state.currentMusic);
+
+  const titleMap: Record<Pages, string> = {
+    home: "Kiwi - 노래가 필요한 순간, 키위",
+    search: "Kiwi - 검색",
+    drawer: "Kiwi - 음악서랍",
+    player: currentMusic
+      ? `${currentMusic.title} - ${currentMusic.singer}`
+      : "Kiwi - 플레이어",
+    music: "Kiwi - 곡 정보",
+    album: "Kiwi - 앨범 정보",
+  };
+
   return (
     <Helmet>
-      <title></title>
+      <title>{titleMap[currentPage]}</title>
     </Helmet>
   );
 }
-
-// head title 동적으로 적용
