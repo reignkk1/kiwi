35b94ec02af69640dcee3b350a9a9b4de57d203e
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-08-28 18:29:31 +0900

    feat: drawer 페이지 헤더 nav 메뉴

---
+++ src/components/pages/drawer/DrawerHeaderNav.tsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { useCurrentPage } from "../../../hooks/useCurrentPage";
+
+export default function DrawerHeaderNav() {
+  const currentPage = useCurrentPage();
+
+  return (
+    <Container>
+      <Ul>
+        <Li $active={currentPage === "playlist"}>
+          <Link to="/playlist">재생목록</Link>
+        </Li>
+        <Li $active={currentPage === "drawer"}>
+          <Link to="/drawer">음악서랍</Link>
+        </Li>
+      </Ul>
+    </Container>
+  );
+}
+
+const Container = styled.nav`
+  margin-bottom: 30px;
+`;
+
+const Ul = styled.ul`
+  display: flex;
+  gap: 15px;
+`;
+
+const Li = styled.li<{ $active: boolean }>`
+  a {
+    color: ${({ $active }) =>
+      $active ? "white" : "rgba(255, 255, 255, 0.3);"};
+    font-size: 20px;
+  }
+`;
