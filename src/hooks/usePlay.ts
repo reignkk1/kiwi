2650943db7ac0bb5108fa59592da4637a35d40a2
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-08-29 18:41:40 +0900

    fix: 현재 페이지가 playlist면 setPlayList 함수 실행안함

--- src/hooks/usePlay.ts
+++ src/hooks/usePlay.ts
@@ -2,11 +2,15 @@ import { useAudioStore } from "../store/audio";
 import { usePlayListStore } from "../store/drawer/usePlayListStore";
 import { useCurrentMusicStore } from "../store/shared";
 import type { MusicType } from "../types";
+import { useCurrentPage } from "./useCurrentPage";
 
 export default function usePlay(music?: MusicType | null) {
+  const currentPage = useCurrentPage();
+
   const setCurrentMusic = useCurrentMusicStore(
     (state) => state.setCurrentMusic
   );
+
   const setIsPlay = useAudioStore((state) => state.setIsPlay);
 
   const playList = usePlayListStore((state) => state.playList);
@@ -14,8 +18,10 @@ export default function usePlay(music?: MusicType | null) {
 
   return () => {
     if (!music) throw new Error("music not found");
+    if (currentPage !== "playlist") {
+      setPlayList([...playList, music.id]);
+    }
 
-    setPlayList([...playList, music.id]);
     setCurrentMusic({ ...music });
     setIsPlay(true);
   };
