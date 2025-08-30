2566bcd226c9a94de33c00b2a41df3258731c5f7
reignkk1 <ruaals456@naver.com>
2025-08-30 23:04:52 +0900

    feat: playlist 페이지가 아닌 곳에서 플레이 버튼 클릭 시 playingIndex 업데이트

--- src/hooks/usePlay.ts
+++ src/hooks/usePlay.ts
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import { useAudioStore } from "../store/audio";
+import { usePlayingIndexStore } from "../store/drawer/usePlayingIndexStore";
 import { usePlayListStore } from "../store/drawer/usePlayListStore";
 import { useCurrentMusicStore } from "../store/shared";
 import type { MusicType } from "../types";
@@ -6,20 +8,27 @@ import { useCurrentPage } from "./useCurrentPage";
 
 export default function usePlay(music?: MusicType | null) {
   const currentPage = useCurrentPage();
-
   const setCurrentMusic = useCurrentMusicStore(
     (state) => state.setCurrentMusic
   );
 
   const setIsPlay = useAudioStore((state) => state.setIsPlay);
+  const playListIds = usePlayListStore((state) => state.playList);
+  const setPlayListIds = usePlayListStore((state) => state.setPlayList);
+  const setPlayingIndex = usePlayingIndexStore(
+    (state) => state.setPlayingIndex
+  );
 
-  const playList = usePlayListStore((state) => state.playList);
-  const setPlayList = usePlayListStore((state) => state.setPlayList);
+  useEffect(() => {
+    if (currentPage !== "playlist") {
+      setPlayingIndex(playListIds.length - 1);
+    }
+  }, [playListIds.length]);
 
   return () => {
     if (!music) throw new Error("music not found");
     if (currentPage !== "playlist") {
-      setPlayList([...playList, music.id]);
+      setPlayListIds([...playListIds, music.id]);
     }
 
     setCurrentMusic({ ...music });
