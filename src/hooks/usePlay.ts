3594284ac07f7577bc1ec388803a5279fb10ace2
reignkk1 <ruaals456@naver.com>
2025-09-04 12:57:09 +0900

    refactor: 만약 drawer, playlist 페이지가 아니라면 playinIndex는 리스트 마지막으로 이동

--- src/hooks/usePlay.ts
+++ src/hooks/usePlay.ts
@@ -1,10 +1,10 @@
 import { useEffect } from "react";
 import { useAudioStore } from "../store/audio";
-import { usePlayingIndexStore } from "../store/drawer/usePlayingIndexStore";
-import { usePlayListStore } from "../store/drawer/usePlayListStore";
 import { useCurrentMusicStore } from "../store/shared";
 import type { MusicType } from "../types";
 import { useCurrentPage } from "./useCurrentPage";
+import { usePlayListStore } from "../store/drawer/usePlayListStore";
+import { useShallow } from "zustand/react/shallow";
 
 export default function usePlay(music?: MusicType | null) {
   const currentPage = useCurrentPage();
@@ -13,21 +13,23 @@ export default function usePlay(music?: MusicType | null) {
   );
 
   const setIsPlay = useAudioStore((state) => state.setIsPlay);
-  const playListIds = usePlayListStore((state) => state.playList);
-  const setPlayListIds = usePlayListStore((state) => state.setPlayList);
-  const setPlayingIndex = usePlayingIndexStore(
-    (state) => state.setPlayingIndex
+  const [playListIds, setPlayListIds, setPlayingIndex] = usePlayListStore(
+    useShallow((state) => [
+      state.playListIds,
+      state.setPlayListIds,
+      state.setPlayingIndex,
+    ])
   );
 
   useEffect(() => {
-    if (currentPage !== "playlist") {
+    if (currentPage !== "drawer" && currentPage !== "playlist") {
       setPlayingIndex(playListIds.length - 1);
     }
   }, [playListIds.length]);
 
   return () => {
     if (!music) throw new Error("music not found");
-    if (currentPage !== "playlist") {
+    if (currentPage !== "drawer" && currentPage !== "playlist") {
       setPlayListIds([...playListIds, music.id]);
     }
 
