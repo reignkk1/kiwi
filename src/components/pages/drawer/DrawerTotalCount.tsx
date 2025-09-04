9fc74d92a70127a425ee718a4bd6cfa6ca6e1a85
reignkk1 <ruaals456@naver.com>
2025-09-04 12:51:58 +0900

    refactor: resolver로 변경

--- src/components/pages/drawer/DrawerTotalCount.tsx
+++ src/components/pages/drawer/DrawerTotalCount.tsx
@@ -2,10 +2,10 @@ import styled from "styled-components";
 import SelectCircle from "./DrawerSelectButton";
 import { useSelectedMusicIndexStore } from "../../../store/drawer";
 import { useShallow } from "zustand/react/shallow";
-import usePlaylistContext from "../../../hooks/usePlaylistResolver";
+import usePlaylistResolver from "../../../hooks/usePlaylistResolver";
 
 export default function DrawerTotalCount() {
-  const { playListIds } = usePlaylistContext();
+  const { playListIds } = usePlaylistResolver();
   const [selectedMusicIds, setSeletedMusicIds] = useSelectedMusicIndexStore(
     useShallow((state) => [
       state.selectedMusicIndex,
