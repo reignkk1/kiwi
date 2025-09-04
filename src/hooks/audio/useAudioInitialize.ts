e185ba35259289c99f2be2e87c9a149774fc3041
reignkk1 <ruaals456@naver.com>
2025-09-04 12:55:56 +0900

    refactor: drawerStore 네이밍 변경

--- src/hooks/audio/useAudioInitialize.ts
+++ src/hooks/audio/useAudioInitialize.ts
@@ -1,12 +1,12 @@
 import { useEffect } from "react";
 import music from "../../musicData.json";
 import { useCurrentMusicStore } from "../../store/shared";
-import { useMusicDrawerStore } from "../../store/drawer/useMusicDrawerStore";
+import { useDrawerStore } from "../../store/drawer/useDrawerStore";
 
 // 오디오 초기음악 설정 hook
 
 export default function useAudioInitialize() {
-  const musicDrawer = useMusicDrawerStore((state) => state.musicDrawer);
+  const playListIds = useDrawerStore((state) => state.playListIds);
 
   const setCurrentMusic = useCurrentMusicStore(
     (state) => state.setCurrentMusic
@@ -14,7 +14,7 @@ export default function useAudioInitialize() {
 
   // 음악서랍에 저장된 첫번째 곡을 default 값으로 초기화
   const defaultMusicInfo = music.data.find(
-    (music) => music.id === musicDrawer[0]
+    (music) => music.id === playListIds[0]
   );
 
   useEffect(() => {
