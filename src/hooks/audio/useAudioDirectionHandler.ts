bb2e26f48cb0370c4d3dd405827ff1bf2f564551
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-09-05 18:39:52 +0900

    feat: 카테고리가 바뀔때마다 뮤직 히스토리 초기화 & 랜덤 재생시에 인덱스 값까지 포함해서 저장

--- src/hooks/audio/useAudioDirectionHandler.ts
+++ src/hooks/audio/useAudioDirectionHandler.ts
@@ -1,9 +1,12 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import type { MusicType } from "../../types";
 import useAudioPlayTypes from "./useAudioPlayTypes";
 import { useAudioStore } from "../../store/audio";
 import { useShallow } from "zustand/react/shallow";
 import { useCurrentMusicStore } from "../../store/shared";
+import usePlayinglistResolver from "../usePlayinglistResolver";
+
+type HistoryType = Array<{ music: MusicType; index: number | null }>;
 
 export default function useAudioDirectionHandler() {
   const { playInOrder, playRandom } = useAudioPlayTypes();
@@ -12,13 +15,24 @@ export default function useAudioDirectionHandler() {
     useShallow((state) => [state.isShuffle, state.setIsPlay])
   );
 
-  const [currentMusic, setCurrentMusic] = useCurrentMusicStore(
-    useShallow((state) => [state.currentMusic, state.setCurrentMusic])
+  const [currentMusic, setCurrentMusic, category] = useCurrentMusicStore(
+    useShallow((state) => [
+      state.currentMusic,
+      state.setCurrentMusic,
+      state.category,
+    ])
   );
 
+  const { playingIndex, setPlayingIndex } = usePlayinglistResolver();
+
   //  음악 히스토리 스택
-  const nextPlayedMusicHistory = useRef<Array<MusicType>>([]).current;
-  const prevPlayedMusicHistory = useRef<Array<MusicType>>([]).current;
+  const nextPlayedMusicHistory = useRef<HistoryType>([]).current;
+  const prevPlayedMusicHistory = useRef<HistoryType>([]).current;
+
+  useEffect(() => {
+    nextPlayedMusicHistory.length = 0;
+    prevPlayedMusicHistory.length = 0;
+  }, [category]);
 
   const handlePlayDirection = (direction: "next" | "prev") => {
     const historyStack =
@@ -29,12 +43,13 @@ export default function useAudioDirectionHandler() {
 
     if (isShuffle) {
       if (historyStack.length === 0) {
-        playHistoryStack.push(currentMusic);
+        playHistoryStack.push({ music: currentMusic, index: playingIndex });
         playRandom();
       } else {
         const popMusicInfo = historyStack.pop()!;
-        playHistoryStack.push(currentMusic);
-        setCurrentMusic(popMusicInfo);
+        playHistoryStack.push({ music: currentMusic, index: playingIndex });
+        setCurrentMusic(popMusicInfo.music);
+        setPlayingIndex(popMusicInfo.index);
         setIsPlay(true);
       }
     } else {
