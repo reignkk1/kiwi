fc4b265d55f002be36b2cf0a4ae7207d214bf9ac
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2025-09-18 18:23:55 +0900

    refactor: 여러 플레이 리스트를 추가해야하므로 객체 형태로 플레이 리스트를 관리

--- src/store/drawer/useDrawerStore.ts
+++ src/store/drawer/useDrawerStore.ts
@@ -5,22 +5,18 @@ export type DrawerStore = {
   category: "drawer";
   playListIds: Record<string, Array<number>>;
   playingIndex: number | null;
-  currentPlaylist: string;
   setPlayListIds: (playList: DrawerStore["playListIds"]) => void;
   setPlayingIndex: (playingIndex: DrawerStore["playingIndex"]) => void;
-  setCurrentPlaylist: (playingIndex: DrawerStore["currentPlaylist"]) => void;
 };
 
 export const useDrawerStore = create(
   persist<DrawerStore>(
     (set) => ({
       category: "drawer",
-      playListIds: {},
-      currentPlaylist: "",
+      playListIds: { test: [] },
       playingIndex: null,
       setPlayListIds: (playListIds) => set({ playListIds }),
       setPlayingIndex: (playingIndex) => set({ playingIndex }),
-      setCurrentPlaylist: (currentPlaylist) => set({ currentPlaylist }),
     }),
     {
       name: "musicDrawer",
